a719d2ec65214dc780bc019e4130069cf647ac83
Gyenh <blazeog@hotmail.com>
2018-07-31 18:12:55 +0200

    Updating the forms

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,6 +4,7 @@ class UserController < ApplicationController
 
 	def create
 
+################# Pour les méthodes form_tag et form ###############
 	  @user = User.new('username' => params[:username],
 	                   'email' => params[:email],
 	                   'bio' => params[:bio])
@@ -13,6 +14,18 @@ class UserController < ApplicationController
 	  else
 	    render '/user/errorpage'
       end
+
+################# Activer cette méthode pour que la form_for fonctionne ###############
+   	  # @user = User.new
+	  # @user.username = params["user"]["username"]
+	  # @user.bio = params["user"]["bio"]
+	  # @user.email = params["user"]["email"]
+
+	  #   if @user.save
+	  #     redirect_to user_path(params["user"][:username])
+	  #   else
+	  #     render '/user/errorpage'
+	  #   end
 	  
 	end
 
